4cb17d6b68caa9e40618a38fc2a37f847184133c
Owner <owner@MacBook-Pro.local>
2017-12-25 17:55:03 -0500

    modifyed server file for production

--- server.js
+++ server.js
@@ -8,6 +8,16 @@ app.get('/home', (req, res) => {
     res.send({Server: 'working'});
 })
 
+if(process.env.NODE_ENV === 'production') {
+    app.use(express.static('client/build'));
+
+    const path = require('path');
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+}
+
+
 app.listen(PORT, () => {
     console.log('Sever started at port: ' + PORT);
 })
