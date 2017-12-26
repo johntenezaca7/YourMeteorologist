cda2d96434c75a502dd0b54a260678723495d071
Owner <owner@MacBook-Pro.local>
2017-12-26 12:42:02 -0500

    Removed https from request url to make the request relative to the current protocol

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { FETCH_WEATHER } from './types';
 
 const API_KEY = '113783f37ed36c4f034dac31031153e3';
-const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+const ROOT_URL = `//api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
 
 
 export function fetchWeather(city) {
