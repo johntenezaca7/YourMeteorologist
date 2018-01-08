07fe9287bf5319320aa8927a57d7da1267a6a998
Owner <owner@MacBook-Pro.local>
2018-01-08 11:00:50 -0500

    added dev and prod keys

--- config/keys.js
+++ config/keys.js
@@ -1,6 +1,8 @@
-module.exports = {
-    googleClientID: '237644938851-dhmsbjs0ok6epa12mf2dl9g1kjnpfqmt.apps.googleusercontent.com',
-    googleClientSecret: 'hREy09SiwOqMspD6gnRZfTi3',
-    mongoURI: 'mongodb://john:coding@ds245277.mlab.com:45277/ur-metoro',
-    cookieKey: 'jskdv98ui23ec87ub1289hiucw89hiun2chiu',
+
+if (process.env.NODE_ENV === 'production') {
+    //We are in production
+    module.exports = require('./prod')
+} else {
+    //We are in development
+    module.exports = require('./dev');
 }
